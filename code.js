11a1c0c54f22b693546b6bdb5db7d76bb9dec551
Viacheslav Hordiienko <viacheslav.hordiienko@pitchbook.com>
2024-01-23 23:29:15 +0200

    update

--- code.js
+++ code.js
@@ -1,11 +1,9 @@
-const status = document.getElementById('status');
-
 window.addEventListener('DOMContentLoaded', () => {
   // Check to see if the API is supported.
   if ('getInstalledRelatedApps' in navigator) {
     check();
   } else {
-    status.innerText = "getInstalledRelatedApps not supported";
+    document.getElementById('status').innerText = "getInstalledRelatedApps not supported";
   }
 });
 
@@ -13,9 +11,9 @@ function check() {
   navigator.getInstalledRelatedApps().then((apps) => {
     const installed = apps.some((app) => app.platform === "play" && app.id === "com.pitchbook.mobile")
     if (installed) {
-      status.innerText = "Installed";
+      document.getElementById('status').innerText = "Installed";
     } else {
-      status.innerText = "Not Installed";
+      document.getElementById('status').innerText = "Not Installed";
     }
   });
 }
