86364fe43553ad641453709242e80013aef6be47
Viacheslav Hordiienko <viacheslav.hordiienko@pitchbook.com>
2024-01-23 23:15:08 +0200

    update js

--- code.js
+++ code.js
@@ -1,35 +1,19 @@
-if (window.location.protocol === 'http:') {
-  const requireHTTPS = document.getElementById('requireHTTPS');
-  const link = requireHTTPS.querySelector('a');
-  link.href = window.location.href.replace('http://', 'https://');
-  requireHTTPS.classList.remove('hidden');
-  window.location.href = 'https:' + window.location.href.substring(5);
-}
-
-const status = document.getElementById('status');
-const ul = document.getElementById('installedApps');
-
-window.addEventListener('load', () => {
+window.addEventListener('DOMContentLoaded', () => {
   // Check to see if the API is supported.
   if ('getInstalledRelatedApps' in navigator) {
-    checkForRelatedApps();
+    check();
   } else {
-    const divNotSupported = document.getElementById('notSupported');
-    divNotSupported.classList.remove('hidden');
+    document.getElementById('notSupported').classList.remove('hidden');
   }
 });
 
-function checkForRelatedApps() {
-  navigator.getInstalledRelatedApps().then((relatedApps) => {
-    status.textContent = `resolved (${relatedApps.length})`;
-    relatedApps.forEach((app) => {
-      const lines = [];
-      lines.push(`<b>id:</b> <code>${app.id}</code>`);
-      lines.push(`<b>platform:</b> ${app.platform}`);
-      lines.push(`<b>url:</b> <a href="${app.url}">${app.url}</a>`);
-      const li = document.createElement('li');
-      li.innerHTML = lines.join('<br>');
-      ul.appendChild(li);
-    });
+function check() {
+  navigator.getInstalledRelatedApps().then((apps) => {
+    const installed = apps.some((app) => app.platform === "play" && app.id === "com.pitchbook.mobile")
+    if (installed) {
+      document.getElementById('status').innerText = "Installed";
+    } else {
+      document.getElementById('status').innerText = "Not Installed";
+    }
   });
 }
