89a809fa59af879fc2386b2971cb0ff8dbcdf09b
Viacheslav Hordiienko <viacheslav.hordiienko@pitchbook.com>
2024-01-24 12:01:55 +0200

    update

--- code.js
+++ code.js
@@ -1,4 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
+  prompt();
+
   // Check to see if the API is supported.
   if ('getInstalledRelatedApps' in navigator) {
     check();
@@ -18,31 +20,33 @@ function check() {
   });
 }
 
-const btnAdd = document.getElementById('prompt');
-let deferredPrompt;
+function prompt() {
+  const btnAdd = document.getElementById('prompt');
+  let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  console.log(e);
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can add to home screen
-  btnAdd.style.display = 'block';
-});
-
-btnAdd.addEventListener('click', (e) => {
-  // hide our user interface that shows our A2HS button
-  btnAdd.style.display = 'none';
-  // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice.then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-      alert('User accepted the A2HS prompt');
-    } else {
-      alert('User dismissed the A2HS prompt');
-    }
-    deferredPrompt = null;
+  window.addEventListener('beforeinstallprompt', (e) => {
+    console.log(e);
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI notify the user they can add to home screen
+    btnAdd.style.display = 'block';
   });
-});
+
+  btnAdd.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    btnAdd.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        alert('User accepted the A2HS prompt');
+      } else {
+        alert('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+}
