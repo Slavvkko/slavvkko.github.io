77845d43e097869957c8ac12d3a98d7c409914b5
Viacheslav Hordiienko <viacheslav.hordiienko@pitchbook.com>
2024-01-24 11:00:29 +0200

    update

--- code.js
+++ code.js
@@ -17,3 +17,31 @@ function check() {
     }
   });
 }
+
+const btnAdd = document.getElementById('prompt');
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  btnAdd.style.display = 'block';
+});
+
+btnAdd.addEventListener('click', (e) => {
+  // hide our user interface that shows our A2HS button
+  btnAdd.style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      alert('User accepted the A2HS prompt');
+    } else {
+      alert('User dismissed the A2HS prompt');
+    }
+    deferredPrompt = null;
+  });
+});
